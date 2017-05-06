5408c55cd41458de5f7638a6aaba959fc69a47de
EricBotter <eric.botter@usi.ch>
2017-05-06 15:19:28 +0200

    Assignment 4

--- src/main/scala/ristretto/drip/PercGen.scala
+++ src/main/scala/ristretto/drip/PercGen.scala
@@ -1,7 +1,10 @@
 package ristretto.drip
 
+import ristretto.drip.DripSyntax._
+
 // Generate Drip from Ristretto ASTs
 object PercGen {
+
   import ristretto.perc.{PercSyntax => P}
   import ristretto.drip.{DripSyntax => D}
   import ristretto.main.{Compiler => Errors}
@@ -26,28 +29,29 @@ object PercGen {
 
   def translate(s: D.Stm): List[P.Stm] = s match {
     case D.Nop() =>
-      P.Nop()::Nil
+      P.Nop() :: Nil
 
     case D.ErrorStm(n) =>
-      P.ErrorStm(n)::Nil
+      P.ErrorStm(n) :: Nil
 
     case D.CJmp(e, tgt) =>
-      // translate to P.CJmp
-      Errors.fatalError(s"translation of $s to Perc is unimplemented")
+      val t = newTemp()
+      translate(e, t) :+ P.CJmp(P.NE(), P.Temp(t), tgt)
 
     case D.Jmp(tgt) =>
-      // translate to P.Jmp
-      Errors.fatalError(s"translation of $s to Perc is unimplemented")
+      P.Jmp(tgt) :: Nil
 
     case D.Store(offset, address, value) =>
-      // translate to P.Store
-      Errors.fatalError(s"translation of $s to Perc is unimplemented")
+      val t1 = newTemp()
+      val t2 = newTemp()
+      translate(address, t1) ++ translate(value, t2) :+
+        P.Store(P.Address(offset, P.Temp(t1)), P.Temp(t2))
 
     case D.Move(t, e) =>
       translate(e, t)
 
     case D.LabelStm(l) =>
-      P.LabelStm(l)::Nil
+      P.LabelStm(l) :: Nil
   }
 
   // Translate the expression `e`, writing the result into temporary `t`
@@ -57,52 +61,112 @@ object PercGen {
       ps ++ translate(e, t)
 
     case D.Call(f, es) =>
-      // Be careful to evaluate all arguments into temporaries *and then*
-      // set all the argument registers. Otherwise a later argument
-      // might overwrite one of the earlier arguments.
-
-      // Evaluate the arguments into new temporaries.
-      // Set the argument registers using P.SetArg.
-      // Then P.Call
-      Errors.fatalError(s"translation of $e to Perc is unimplemented")
+      val temps = es.indices.map(i => newTemp())
+      val stms = es.indices.map(i => translate(es(i), temps(i)))
+      val regset = es.indices.map(i => P.SetArg(i, P.Temp(temps(i))))
+      stms.flatten.toList ++ regset :+
+        P.Call(t, P.Global(f.asInstanceOf[D.Global].label))
 
     case D.Alloc(sz) =>
-      // Translate into a call to "ristretto_alloc"
       val tsz = newTemp()
-      translate(sz, tsz) :+ P.SetArg(0, P.Temp(tsz)) :+ P.Call(t, P.Global("ristretto_alloc"))
+      translate(sz, tsz) :+
+        P.SetArg(0, P.Temp(tsz)) :+
+        P.Call(t, P.Global("ristretto_alloc"))
 
     case D.Load(offset, addr) =>
-      // Translate into P.Load
-      Errors.fatalError(s"translation of $e to Perc is unimplemented")
+      val t1 = newTemp()
+      translate(addr, t1) :+ P.Load(t, P.Address(offset, P.Temp(t1)))
 
     case D.BinOp(op, e1, e2) =>
-      // Arithmetic expressions are straightforward.
-      //
-      // But comparisons are translated into a conditional jump.
-      // Here's one possible translation for EQ:
-      // t := [[ EQ e1 e2 ]] =
-      //   t1 := [[ e1 ]]
-      //   t2 := [[ e2 ]]
-      //   t3 := SUB t1 t2
-      //   move t = 1
-      //   cjmp EQ t3 0 L
-      //   move t = 0
-      //   L:
-      //
-      // t := [[ ADD e1 e2 ]]
-      Errors.fatalError(s"translation of $e to Perc is unimplemented")
+      val t1 = newTemp()
+      val t2 = newTemp()
+      val r1 = translate(e1, t1)
+      val r2 = translate(e2, t2)
+      val r3 = op match {
+        case ADD() =>
+          P.Bin(t, P.ADD(), P.Temp(t1), P.Temp(t2)) :: Nil
+        case SUB() =>
+          P.Bin(t, P.SUB(), P.Temp(t1), P.Temp(t2)) :: Nil
+        case MUL() =>
+          P.Bin(t, P.MUL(), P.Temp(t1), P.Temp(t2)) :: Nil
+        case DIV() =>
+          P.Bin(t, P.DIV(), P.Temp(t1), P.Temp(t2)) :: Nil
+        case REM() =>
+          P.Bin(t, P.REM(), P.Temp(t1), P.Temp(t2)) :: Nil
+        case LT() =>
+          val tx = newTemp()
+          val l1 = newLabel()
+          List(
+            P.Bin(tx, P.SUB(), P.Temp(t1), P.Temp(t2)),
+            P.Move(t, P.Lit(1)),
+            P.CJmp(P.LT(), P.Temp(tx), l1),
+            P.Move(t, P.Lit(0)),
+            P.LabelStm(l1)
+          )
+        case GT() =>
+          val tx = newTemp()
+          val l1 = newLabel()
+          List(
+            P.Bin(tx, P.SUB(), P.Temp(t1), P.Temp(t2)),
+            P.Move(t, P.Lit(1)),
+            P.CJmp(P.GT(), P.Temp(tx), l1),
+            P.Move(t, P.Lit(0)),
+            P.LabelStm(l1)
+          )
+        case LE() =>
+          val tx = newTemp()
+          val l1 = newLabel()
+          List(
+            P.Bin(tx, P.SUB(), P.Temp(t1), P.Temp(t2)),
+            P.Move(t, P.Lit(1)),
+            P.CJmp(P.LE(), P.Temp(tx), l1),
+            P.Move(t, P.Lit(0)),
+            P.LabelStm(l1)
+          )
+        case GE() =>
+          val tx = newTemp()
+          val l1 = newLabel()
+          List(
+            P.Bin(tx, P.SUB(), P.Temp(t1), P.Temp(t2)),
+            P.Move(t, P.Lit(1)),
+            P.CJmp(P.GE(), P.Temp(tx), l1),
+            P.Move(t, P.Lit(0)),
+            P.LabelStm(l1)
+          )
+        case EQ() =>
+          val tx = newTemp()
+          val l1 = newLabel()
+          List(
+            P.Bin(tx, P.SUB(), P.Temp(t1), P.Temp(t2)),
+            P.Move(t, P.Lit(1)),
+            P.CJmp(P.EQ(), P.Temp(tx), l1),
+            P.Move(t, P.Lit(0)),
+            P.LabelStm(l1)
+          )
+        case NE() =>
+          val tx = newTemp()
+          val l1 = newLabel()
+          List(
+            P.Bin(tx, P.SUB(), P.Temp(t1), P.Temp(t2)),
+            P.Move(t, P.Lit(1)),
+            P.CJmp(P.NE(), P.Temp(tx), l1),
+            P.Move(t, P.Lit(0)),
+            P.LabelStm(l1)
+          )
+      }
+      r1 ++ r2 ++ r3
 
     case D.Temp(x) if x == t =>
       Nil
 
     case D.Temp(x) =>
-      P.Move(t, P.Temp(x))::Nil
+      P.Move(t, P.Temp(x)) :: Nil
 
     case D.Lit(n) =>
-      P.Move(t, P.Lit(n))::Nil
+      P.Move(t, P.Lit(n)) :: Nil
 
     case D.Global(x) =>
-      P.Move(t, P.Global(x))::Nil
+      P.Move(t, P.Global(x)) :: Nil
   }
 
   def newLabel(): Label = {
